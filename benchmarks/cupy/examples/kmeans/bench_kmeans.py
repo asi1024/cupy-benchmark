6b95c676fe2002ec21effa90895fb56e57e8ccab
Akifumi Imanishi <imanishi@preferred.jp>
2018-06-14 15:37:23 +0900

    Add benchmarks for kmeans

---
+++ benchmarks/cupy/examples/kmeans/bench_kmeans.py
@@ -0,0 +1,21 @@
+from benchmarks.numpy.common import Benchmark
+from benchmarks.utils import sync
+from benchmarks.utils.helper import parameterize
+
+import cupy
+
+from .kmeans import fit
+
+
+@sync
+@parameterize([('n_clusters', [1, 2]),
+               ('num', [1, 5000000]),
+               ('max_iter', [10]),
+               ('use_custom_kernel', [True, False])])
+class KMeans(Benchmark):
+    def setup(self, n_clusters, num, max_iter, use_custom_kernel):
+        samples = cupy.random.randn(num, 2).astype(cupy.float32)
+        self.X_train = cupy.r_[samples + 1, samples - 1]
+
+    def time_kmeans(self, n_clusters, num, max_iter, use_custom_kernel):
+        fit(self.X_train, n_clusters, max_iter, use_custom_kernel)
